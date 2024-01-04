f47b4e8a8928f55229baf3ae55044983446ce421
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-01-04 19:28:57 +0530

    complete backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const port = 3001;
 const host = '127.0.0.1';
 const mongoose = require('mongoose');
+const router = require('./router');
 
 app.use(cors());
 app.use(express.json());
@@ -26,3 +27,5 @@ connect();
 const server = app.listen(port, host, ()=>{
     console.log(`Node server is listening to ${server.address().port}`)
 });
+
+app.use('/api', router);
