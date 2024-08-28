e09a255a57453c5c53fad2b08da9f7fbb419a032
Prasadini V.G.H. <prasadinivgh.21@uom.lk>
2024-08-28 22:47:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 const host = 'localhost';
 const mongoose = require('mongoose');
 const router = require('./router');
